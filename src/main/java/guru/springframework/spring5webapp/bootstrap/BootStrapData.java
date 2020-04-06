f7e5e1e88d4c053837e4d58c67300a72b23a3c8d
Lior Lavon <>
2020-04-06 20:17:50 +0200

    Publisher entity , PublisherRepository, dump data created

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,4 +1,61 @@
 package guru.springframework.spring5webapp.bootstrap;
 
-public class BootStrapData {
+import guru.springframework.spring5webapp.models.Author;
+import guru.springframework.spring5webapp.models.Book;
+import guru.springframework.spring5webapp.models.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+// Spring is going to look for instances of this type, when he fineds them he will run them
+@Component // Spring managed component
+public class BootStrapData implements CommandLineRunner {
+
+    // this is dependency injection
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        // setup default data
+
+        System.out.println("Started in Bootstrap");
+
+        Publisher publisher = new Publisher("Book publisher", "Poortwachter 11", "Amsterdam", "North Holand", "1186MB");
+        publisherRepository.save(publisher);
+
+
+        Author eric = new Author("Eric", "Evens");
+        Book ddd = new Book("Domain driven design", "12345");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        // ---------
+
+        Author rad = new Author("Rad", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "6576576987");
+        rad.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rad);
+
+        authorRepository.save(rad);
+        bookRepository.save(noEJB);
+
+
+
+        System.out.println("Number of books: " + bookRepository.count());
+        System.out.println("Publisher count:" + publisherRepository.count());
+    }
 }
